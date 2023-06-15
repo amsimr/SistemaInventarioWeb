4bb6cc2739fd0d9d73e6a6077181a3503e7a3332
amsimr <114847931+amsimr@users.noreply.github.com>
2023-06-15 18:27:44 -0400

    Patron de Repositorio

---
+++ SistemaInventarioWeb.AccesoDatos/Repositorio/BodegaRepositorio.cs
@@ -0,0 +1,33 @@
+﻿using SistemaInventarioWeb.AccesoDatos.Data;
+using SistemaInventarioWeb.AccesoDatos.Repositorio.IRepositorio;
+using SistemaInventarioWeb.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaInventarioWeb.AccesoDatos.Repositorio
+{
+    public class BodegaRepositorio : Repositorio<Bodega>, IBodegaRepositorio
+    {
+        private readonly ApplicationDbContext _db;
+
+        public BodegaRepositorio(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Actualizar(Bodega bodega)
+        {
+            var bodegaBD = _db.Bodegas.FirstOrDefault(b => b.Id == bodega.Id);
+            if (bodegaBD != null)
+            {
+                bodegaBD.Nombre = bodega.Nombre;
+                bodegaBD.Descripcion = bodega.Descripcion;
+                bodegaBD.Estado = bodega.Estado;
+                _db.SaveChanges();
+            }
+        }
+    }
+}
